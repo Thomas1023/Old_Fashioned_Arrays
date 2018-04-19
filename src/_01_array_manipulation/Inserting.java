c86c9a885963619e6cf1ed5ac5a1045b3bc3204e
Chloe Luwa <chloeluwa@yahoo.com>
2018-04-18 19:55:21 -0700

    (ip)
    open

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,36 +1,45 @@
 package _01_array_manipulation;
 
-
-
 public class Inserting {
 
 	public static int[] insertAt(int[] testArray, int i, int j) {
-		testArray[i]=j;		
+		int test = 0;
+		test = testArray[i];
+		// test1 = testArray[j];
+
+		testArray[i] = testArray[j];
+		testArray[j] = test;
+		System.err.println("from" + testArray[j]);
+		System.err.println("from" + testArray[i]);
+
 		return testArray;
-				
+
 	}
 
 	public static String[] insertAlphabetically(String[] orderedArray, String string) {
-		String temp1="";
-		
-		
-		for (int i = 0; i < orderedArray.length; i++) {
-			for (int e = i+1; e < orderedArray.length; e++) {
-				if(orderedArray[i].compareTo(orderedArray[e])>0) {
-					temp1=orderedArray[i];
-					orderedArray[i]=orderedArray[e];
-					orderedArray[e]=temp1;
-					
-				}else {
-					
+		String temp1 = "";	//Bubble sort holder
+
+		String[] tArray = new String[orderedArray.length + 1]; //new array with included variable
+		for (int i = 0; i < orderedArray.length; i++) { //iterate thru array to reconstruct it
+			tArray[i] = orderedArray[i]; 				//Moving assets of old array
+		}
+		tArray[orderedArray.length + 1] = string;	//adding new string to array
+
+		for (int i = 0; i < tArray.length; i++) {			//
+			for (int e = i + 1; e < tArray.length; e++) {    //
+				if (tArray[i].compareTo(tArray[e]) > 0) {    //
+					temp1 = tArray[i];						//Bubble Sort
+					tArray[i] = tArray[e];					//
+					tArray[e] = temp1;						//
+															//
+				} else {
+
 				}
 			}
 		}
-		
-		
-		return orderedArray;
-		
+
+		return tArray;//returns sorted
 
 	}
-	
+
 }
--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,5 +1,18 @@
 package _02_diy_array_list;
 
-public class ArrayList{
+public class ArrayList<T>{
+	T[] list;
+	public void add(T) {
+		
+		list =
+	}
+
+	public Object get(T i) {
+		return 
+	}
+
+	public void set(int i, T string) {
+		list.set(i, string);
+	}
 
 }

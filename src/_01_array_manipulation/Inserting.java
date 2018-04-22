22d1d267e9b09f2858fe755a245fda6b0272b739
Thomas1023 <thomas.steiner03@gmail.com>
2018-04-22 11:12:18 -0700

    done w/ array pt1 (ip) 2nd

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -17,29 +17,17 @@ public class Inserting {
 	}
 
 	public static String[] insertAlphabetically(String[] orderedArray, String string) {
-		String temp1 = "";	//Bubble sort holder
-
-		String[] tArray = new String[orderedArray.length + 1]; //new array with included variable
-		for (int i = 0; i < orderedArray.length; i++) { //iterate thru array to reconstruct it
-			tArray[i] = orderedArray[i]; 				//Moving assets of old array
+		String[] TempArray = new String[orderedArray.length + 1];
+		for (int e = 0; e < orderedArray.length; e++) {
+			TempArray[e] = orderedArray[e];
 		}
-		tArray[orderedArray.length + 1] = string;	//adding new string to array
-
-		for (int i = 0; i < tArray.length; i++) {			//
-			for (int e = i + 1; e < tArray.length; e++) {    //
-				if (tArray[i].compareTo(tArray[e]) > 0) {    //
-					temp1 = tArray[i];						//Bubble Sort
-					tArray[i] = tArray[e];					//
-					tArray[e] = temp1;						//
-															//
-				} else {
-
-				}
-			}
-		}
-
-		return tArray;//returns sorted
-
+		
+		TempArray[TempArray.length - 1] = string;
+		
+		Sorting.sort(TempArray);
+		
+		return TempArray;
 	}
+
 
 }
--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,18 +1,62 @@
 package _02_diy_array_list;
 
 public class ArrayList<T>{
-	T[] list;
-	public void add(T) {
+	public boolean contains(String string) {
+		return false;
+	}
+	public void remove(int i) {
+		// TODO Auto-generated method stub
 		
-		list =
+	}
+	T[] Ts = (T[]) new Object[0];
+	int Temp = Ts.length;
+	public void add(Object i) {
+		Object[] temp = new Object[Ts.length];
+	 for (int j = 0; j < Ts.length; j++) {
+		temp[j] = Ts[j];
+	 }
+	 
+	 Ts = (T[]) new Object[Ts.length + 1];
+	 
+	 for (int j = 0; j < temp.length; j++) {
+			Ts[j] = (T) temp[j];
+	 }
+	 Ts[Ts.length - 1] = (T) i;	//reshape
+	 Temp = Ts.length;
+	}
+	
+	public ArrayList(){
+		
 	}
 
-	public Object get(T i) {
-		return 
+	public T get(int i) {
+		
+		return (T) Ts[i];
 	}
 
-	public void set(int i, T string) {
-		list.set(i, string);
+	
+
+	public void addAt(int i, Object c) {
+		T[] Tempray = (T[]) new Object[Ts.length + 1];
+		for (int h = 0; h < i; h++) {
+			Tempray[h] = Ts[h];
+		}
+		Tempray[i] = (T) c;
+		for (int e = 0; e < Ts.length - i; e++) {
+			Tempray[e + i + 1] = Ts[e + i];
+		}
+		Ts = (T[]) new Object[Ts.length + 1];
+		Ts = Tempray;
+		
 	}
+public void set(int i, Object string) {
+		Ts[i] = (T)string;
+		
+	}
+
+
+
+
+	
 
 }
